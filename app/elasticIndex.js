f09a6b6eb9fabaaf259871a335681e08a7d6e394
nodlam <nodlam@live.fr>
2018-03-25 03:08:14 +0200

    route + suv

--- app/elasticIndex.js
+++ app/elasticIndex.js
@@ -1,8 +1,9 @@
 const jsonfile = require('jsonfile');
 var client = require('../app/connectElastic.js');
+const express = require('express')
+const app = express()
 
-module.exports.pop = function() {
-  jsonfile.readFile(__dirname+'/car.json', function(err, res) {
+jsonfile.readFile(__dirname+'/car.json', function(err, res) {
     if(err) console.error(err);
     else {
       res.forEach((model, i) => {
@@ -19,4 +20,33 @@ module.exports.pop = function() {
       })
     }
   })
-}
+
+
+app.get('/suv', function(req,res){
+
+    client.search({
+        index: 'caradisiac',
+        type: 'model',
+        body:{
+            "size":400,
+            "sort":[
+                {"volume.keyword" :{"order":"desc"}}
+            ]
+        }
+    },function (error, response,status) {
+        if (error){
+            console.log("ERROR : "+error)
+        }
+        else {
+            res.send(response.hits.hits);
+        }
+    });
+
+})
+
+var port = 9200;
+app.listen(port, function () {
+    console.log('App listening on port ' + port + '!')
+})
+
+
